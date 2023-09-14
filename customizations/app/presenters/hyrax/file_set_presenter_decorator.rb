5fd6878fb53363aef0286dcef4abb5901990a335
Scott Tyson <st1072@georgetown.edu>
2023-09-14 18:04:13 -0400

    Add file size to file set characterization and list on work show page

--- customizations/app/presenters/hyrax/file_set_presenter_decorator.rb
+++ customizations/app/presenters/hyrax/file_set_presenter_decorator.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
-# OVERRIDE Hyrax 3.5.0, seems to be needed for :vtt? and :language_code to work from the SolrDocument
 module Hyrax
-    module FileSetPresenterDecorator
-      def vtt?
-        solr_document.vtt?
-      end
-  
-      def language_code
-        solr_document.language_code
-      end
+  module FileSetPresenterDecorator
+    include ActionView::Helpers::NumberHelper
+    
+    # Formats the bytes in file_size into a more understandable representation (e.g., giving it 1500 yields 1.5 KB)
+    def file_size
+      number_to_human_size(solr_document.file_size, precision: 2)
+    end
+
+    def vtt?
+      solr_document.vtt?
+    end
+
+    def language_code
+      solr_document.language_code
     end
   end
-  
-  Hyrax::FileSetPresenter.prepend(Hyrax::FileSetPresenterDecorator)
+end
+
+Hyrax::FileSetPresenter.prepend(Hyrax::FileSetPresenterDecorator)
