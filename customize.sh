1637cddd0f6f44d420dcbd1947806f4437e62d32
Scott Tyson <st1072@georgetown.edu>
2023-10-04 13:53:52 -0400

    Make further changes to deploy scripts

--- customize.sh
+++ customize.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+function error_fail {
+	echo "ERROR: $1"
+	echo "Exiting deploy."
+	exit 1
+}
+
 source setenv.sh
+if [ $? != 0 ]; then
+	error_fail "Failed to load 'setenv.sh' and set environment variables."
+fi
 
 cp -R customizations/. $HYKU_DIR
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+function error_fail {
+	echo "ERROR: $1"
+	echo "Exiting deploy."
+	exit 1
+}
+
 source setenv.sh
+if [ $? != 0 ]; then
+	error_fail "Failed to load 'setenv.sh' and set environment variables."
+fi
 
+rm -rf ${HYKU_DIR}.old
+mkdir -p $HYKU_DIR 
+mv $HYKU_DIR ${HYKU_DIR}.old
+if [ $? != 0 ]; then
+	error_fail "Failed to backup ${HYKU_DIR}."
+fi
 rm -rf $HYKU_DIR
-
+mkdir $HYKU_DIR
 cp -R hyku/. $HYKU_DIR
 
 source customize.sh
