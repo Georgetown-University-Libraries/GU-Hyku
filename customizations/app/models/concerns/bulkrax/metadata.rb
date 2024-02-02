056aa5752381d580c6003707b9e9991bcd2296ce
Scott Tyson <st1072@georgetown.edu>
2024-02-01 19:20:07 -0500

    Correct indentation for some model files

--- customizations/app/models/concerns/bulkrax/metadata.rb
+++ customizations/app/models/concerns/bulkrax/metadata.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 module Bulkrax
-    module Metadata
-      extend ActiveSupport::Concern
+  module Metadata
+    extend ActiveSupport::Concern
   
-      included do
-        #TODO: Replace repository.libary.georgetown.edu with new DG domain (once live)
-        property :bulkrax_id, predicate: ::RDF::URI.new("http://repository.library.georgetown.edu/ns#bulkrax_id"), multiple: false do |index|
-          index.as :stored_searchable
-        end
+    included do
+      #TODO: Replace repository.libary.georgetown.edu with new DG domain (once live)
+      property :bulkrax_id, predicate: ::RDF::URI.new("http://repository.library.georgetown.edu/ns#bulkrax_id"), multiple: false do |index|
+        index.as :stored_searchable
       end
     end
   end
+end
