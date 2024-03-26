7fd813952e7c224997eb7793940feaa9b26feaea
Scott Tyson <st1072@georgetown.edu>
2024-03-26 18:59:44 -0400

    Add users to GU Depositors group by default

---
+++ customizations/app/models/user.rb
@@ -0,0 +1,137 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Includes lib/rolify from the rolify gem
+  rolify
+  # Connects this user object to Hydra behaviors.
+  include Hydra::User
+  # Connects this user object to Hyrax behaviors.
+  include Hyrax::User
+  include Hyrax::UserUsageStats
+
+  attr_accessible :email, :password, :password_confirmation if Blacklight::Utils.needs_attr_accessible?
+  # Connects this user object to Blacklights Bookmarks.
+  include Blacklight::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :invitable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: %i[saml openid_connect cas]
+
+  after_create :add_default_group_membership!
+
+  # set default scope to exclude guest users
+  def self.default_scope
+    where(guest: false)
+  end
+
+  scope :for_repository, -> {
+    joins(:roles)
+  }
+
+  scope :registered, -> { for_repository.group(:id).where(guest: false) }
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth&.info&.email || [auth.uid, '@', Site.instance.account.email_domain].join if user.email.blank?
+      user.password = Devise.friendly_token[0, 20]
+      user.display_name = auth&.info&.name # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+  # Method added by Blacklight; Blacklight uses #to_s on your
+  # user class to get a user-displayable login/identifier.
+  def to_s
+    email
+  end
+
+  def is_admin
+    has_role?(:admin) || has_role?(:admin, Site.instance)
+  end
+
+  def is_superadmin
+    has_role? :superadmin
+  end
+
+  # This comes from a checkbox in the proprietor interface
+  # Rails checkboxes are often nil or "0" so we handle that
+  # case directly
+  def is_superadmin=(value)
+    value = ActiveModel::Type::Boolean.new.cast(value)
+    if value
+      add_role :superadmin
+    else
+      remove_role :superadmin
+    end
+  end
+
+  def site_roles
+    roles.site
+  end
+
+  def site_roles=(roles)
+    roles.reject!(&:blank?)
+
+    existing_roles = site_roles.pluck(:name)
+    new_roles = roles - existing_roles
+    removed_roles = existing_roles - roles
+
+    new_roles.each do |r|
+      add_role r, Site.instance
+    end
+
+    removed_roles.each do |r|
+      remove_role r, Site.instance
+    end
+  end
+
+  # Hyrax::Group memberships are tracked through User#roles. This method looks up
+  # the Hyrax::Groups the user is a member of and returns each one in an Array.
+  # Example:
+  #   u = User.last
+  #   u.roles
+  #   => #<ActiveRecord::Associations::CollectionProxy [#<Role id: 8, name: "member",
+  #      resource_type: "Hyrax::Group", resource_id: 2,...>]>
+  #   u.hyrax_groups
+  #   => [#<Hyrax::Group id: 2, name: "registered", description: nil,...>]
+  def hyrax_groups
+    roles.where(name: 'member', resource_type: 'Hyrax::Group').map(&:resource).uniq
+  end
+
+  # Override method from hydra-access-controls v11.0.0 to use Hyrax::Groups.
+  # NOTE: DO NOT RENAME THIS METHOD - it is required for permissions to function properly.
+  # @return [Array] Hyrax::Group names the User is a member of
+  def groups
+    hyrax_groups.map(&:name)
+  end
+
+  # NOTE: This is an alias for #groups to clarify what the method is doing.
+  # This is necessary because #groups overrides a method from a gem.
+  # @return [Array] Hyrax::Group names the User is a member of
+  def hyrax_group_names
+    groups
+  end
+
+  # TODO: this needs tests and to be moved to the service
+  # Tmp shim to handle bug
+  def group_roles
+    hyrax_groups.map(&:roles).flatten.uniq
+  end
+
+  # TODO: The current way this method works may be problematic; if a User signs up
+  # in the global tenant, they won't get group memberships for any tenant. Need to
+  # identify all the places this kind of situation can arise (invited users, etc)
+  # and decide what to do about it.
+  def add_default_group_membership!
+    return if guest?
+    return if Account.global_tenant?
+
+    Hyrax::Group.find_or_create_by!(name: Ability.registered_group_name).add_members_by_id(id)
+    #Add the user to the GU Depositors group by default
+    Hyrax::Group.find_or_create_by!(name: "gu_depositors").add_members_by_id(id)
+  end
+end
