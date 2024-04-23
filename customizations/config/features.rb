987d90c8becee3f06ec49a48ff008ce1e0b5e935
Scott Tyson <st1072@georgetown.edu>
2024-04-23 13:00:16 -0400

    On homepage, exclude featured researcher, share button, and featured works elements by default

--- customizations/config/features.rb
+++ customizations/config/features.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
 Flipflop.configure do
-    feature :use_pdf_viewer,
-            default: true,
-            description: "Use embedded PDF viewer instead of displaying thumbnail and action buttons"
-    feature :show_featured_researcher,
-            default: true,
-            description: "Shows the Featured Researcher tab on the homepage."
+  feature :use_pdf_viewer,
+          default: true,
+          description: "Use embedded PDF viewer instead of displaying thumbnail and action buttons"
   
-    feature :show_share_button,
-            default: true,
-            description: "Shows the 'Share Your Work' button on the homepage."
-  
-    feature :show_featured_works,
-            default: true,
-            description: "Shows the Featured Works tab on the homepage."
-  
-    feature :show_recently_uploaded,
-            default: true,
-            description: "Shows the Recently Uploaded tab on the homepage."
+  feature :show_featured_researcher,
+          default: false,
+          description: "Shows the Featured Researcher tab on the homepage."
+
+  feature :show_share_button,
+          default: false,
+          description: "Shows the 'Share Your Work' button on the homepage."
+
+  feature :show_featured_works,
+          default: false,
+          description: "Shows the Featured Works tab on the homepage."
+
+  feature :show_recently_uploaded,
+          default: true,
+          description: "Shows the Recently Uploaded tab on the homepage."
   end
   
