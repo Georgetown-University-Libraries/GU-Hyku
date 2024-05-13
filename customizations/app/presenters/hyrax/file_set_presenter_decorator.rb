058f8d397db1653f70a3b9a23bd5dbd4a5e788ed
Scott Tyson <st1072@georgetown.edu>
2024-05-13 18:55:53 -0400

    On work show page, customize file set values and links in Items panel

--- customizations/app/presenters/hyrax/file_set_presenter_decorator.rb
+++ customizations/app/presenters/hyrax/file_set_presenter_decorator.rb
@@ -13,6 +13,36 @@ module Hyrax
       solr_document.vtt?
     end
 
+    def extent
+      if solr_document.duration
+        time_to_words(solr_document.duration&.first)
+      elsif solr_document.page_count
+        "#{solr_document.page_count&.first} pages"
+      elsif solr_document.width && solr_document.height
+        "#{solr_document.width}×#{solr_document.height} pixels"
+      else
+        "--"
+      end
+    end
+
+    def time_to_words(time_str)
+      # Parse time string into hours, minutes, and seconds
+      hours, minutes, seconds = time_str.split(':').map(&:to_i)
+    
+      components = []
+    
+      components << "#{hours} hours" if hours > 0
+    
+      components << "#{minutes} minutes" if minutes > 0
+    
+      components << "#{seconds} seconds" if seconds > 0
+    
+      result = components.join(', ')
+    
+      # Format the result
+      result
+    end
+
     def language_code
       solr_document.language_code
     end
