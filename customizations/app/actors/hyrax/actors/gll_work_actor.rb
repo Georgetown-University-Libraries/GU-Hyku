4547e7b2295e96123a30bc0d2279920b3884095f
Scott Tyson <st1072@georgetown.edu>
2024-05-20 16:58:55 -0400

    Assign handles to works if missing on create

--- customizations/app/actors/hyrax/actors/gll_work_actor.rb
+++ customizations/app/actors/hyrax/actors/gll_work_actor.rb
@@ -3,6 +3,13 @@
 module Hyrax
   module Actors
     class GllWorkActor < Hyrax::Actors::BaseActor
+      # Assigns handle, when missing, according to first HandleCount record
+      def create(env)
+        if env.attributes.with_indifferent_access[:handle].blank?
+          env.curation_concern.handle = ::HandleCount.first.increment_count.to_s
+        end
+        super
+      end
     end
   end
 end
--- customizations/app/actors/hyrax/actors/guetd_actor.rb
+++ customizations/app/actors/hyrax/actors/guetd_actor.rb
@@ -1,9 +1,16 @@
 # Generated via
 #  `rails generate hyrax:work Guetd`
 module Hyrax
-    module Actors
-      class GuetdActor < Hyrax::Actors::BaseActor
+  module Actors
+    class GuetdActor < Hyrax::Actors::BaseActor
+      # Assigns handle, when missing, according to first HandleCount record
+      def create(env)
+        if env.attributes.with_indifferent_access[:handle].blank?
+          env.curation_concern.handle = ::HandleCount.first.increment_count.to_s
+        end
+        super
       end
     end
   end
+end
   
